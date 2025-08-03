f835add012ceac8e3ac54540da78d14cb99521f1
santidele22 <santiidele21@gmail.com>
2025-08-03 16:08:23 -0300

    add interfaces a config ts

---
+++ src/app/mockup/contacts.ts
@@ -0,0 +1,11 @@
+import { Contact } from "@interaces/contact";
+export const CONTACTS: Contact[] = [
+  {
+    text: "santiidele21@gmail.com",
+    icon: "icons/email.svg"
+  },
+  {
+    text: "+54 2944-667161",
+    icon: "icons/phone.svg"
+  }
+]
---
+++ src/app/mockup/social-media.ts
@@ -0,0 +1,24 @@
+import { Social } from "@interaces/social";
+
+export const SOCIAL_MEDIA: Social[] = [
+  {
+    social: "instagram",
+    href: ""
+  },
+  {
+    social: "linkedin",
+    href: ""
+
+  },
+  {
+    social: "github",
+    href: ""
+
+  },
+  {
+    social: "portafolio",
+    href: ""
+
+
+  }
+]
